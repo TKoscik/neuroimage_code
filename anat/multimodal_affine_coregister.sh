8aac6badafcd23b4c86cf29d85d45184caf520f2
koscikt <timothy-koscik@uiowa.edu>
2020-02-26 09:23:50 -0600

    Update dicom/qc_dicom_conversion.R, anat/multimodal_affine_coregister.sh files

---
+++ anat/multimodal_affine_coregister.sh
@@ -0,0 +1,125 @@
+#!/bin/bash -e
+
+#===============================================================================
+# Registration of images to a subject's base image
+# Authors: Timothy R. Koscik, PhD
+# Date: 2020-02-25
+#===============================================================================
+
+# Parse inputs -----------------------------------------------------------------
+OPTS=`getopt -o hcvk --long researcher:,project:,group:,subject:,session:,prefix:,\
+other-inputs:,template:,space:,dir-scratch:,dir-nimgcore:,dir-pincsource:,\
+help,dry-run,verbose,keep -n 'parse-options' -- "$@"`
+if [ $? != 0 ]; then
+  echo "Failed parsing options" >&2
+  exit 1
+fi
+eval set -- "$OPTS"
+
+DATE_SUFFIX=$(date +%Y%m%dT%H%M%S)
+RESEARCHER=
+PROJECT=
+GROUP=
+SUBJECT=
+SESSION=
+PREFIX=
+OTHER_INPUTS=
+TEMPLATE=HCPICBM
+SPACE=1mm
+DIR_SCRATCH=/Shared/inc_scratch/scratch_${DATE_SUFFIX}
+DIR_NIMGCORE=/Shared/nopoulos/nimg_core
+DIR_PINCSOURCE=/Shared/pinc/sharedopt/apps/sourcefiles
+HELP=false
+DRY_RUN=false
+VERBOSE=0
+KEEP=false
+
+while true; do
+  case "$1" in
+    -h | --help) HELP=true ; shift ;;
+    -c | --dry-run) DRY-RUN=true ; shift ;;
+    -v | --verbose) VERBOSE=1 ; shift ;;
+    -k | --keep) KEEP=true ; shift ;;
+    --researcher) RESEARCHER="$2" ; shift 2 ;;
+    --project) PROJECT="$2" ; shift 2 ;;
+    --group) GROUP="$2" ; shift 2 ;;
+    --subject) SUBJECT="$2" ; shift 2 ;;
+    --session) SESSION="$2" ; shift 2 ;;
+    --prefix) PREFIX="$2" ; shift 2 ;;
+    --other-inputs) OTHER_INPUTS="$2" ; shift 2 ;;
+    --template) TEMPLATE="$2" ; shift 2 ;;
+    --space) SPACE="$2" ; shift 2 ;;
+    --dir-scratch) SCRATCH="$2" ; shift 2 ;;
+    --dir-nimgcore) DIR_NIMGCORE="$2" ; shift 2 ;;
+    --dir-pincsource) DIR_PINCSOURCE="$2" ; shift 2 ;;
+    -- ) shift ; break ;;
+    * ) break ;;
+  esac
+done
+
+# Usage Help -------------------------------------------------------------------
+if [[ "${HELP}" == "true" ]]; then
+  FUNC_NAME=(`basename "$0"`)
+  echo ''
+  echo '------------------------------------------------------------------------'
+  echo "Iowa Neuroimage Processing Core: ${FUNC_NAME}"
+  echo 'Author: <<author names>>'
+  echo 'Date:   <<date of authorship>>'
+  echo '------------------------------------------------------------------------'
+  echo "Usage: ${FUNC_NAME}"
+  echo '  -h | --help              display command help'
+  echo '  -c | --dry-run           test run of function'
+  echo '  -v | --verbose           add verbose output to log file'
+  echo '  -k | --keep              keep preliminary processing steps'
+  echo '  --researcher <value>     directory containing the project,'
+  echo '                           e.g. /Shared/koscikt'
+  echo '  --project <value>        name of the project folder, e.g., iowa_black'
+  echo '  --group <value>          group permissions for project,'
+  echo '                           e.g., Research-kosciklab'
+  echo '  --subject <value>        subject identifer, e.g., 123'
+  echo '  --session <value>        session identifier, e.g., 1234abcd'
+  echo '  --prefix <value>         scan prefix,
+  echo '                           'e.g., sub-123_ses-1234abcd_acq-MPRAGE_T1w'
+  echo '  --other-inputs <value>   other inputs necessary for function'
+  echo '  --template <value>       name of template to use (if necessary),'
+  echo '                           e.g., HCPICBM'
+  echo '  --space <value>          spacing of template to use, e.g., 1mm'
+  echo '  --dir-scratch <value>    directory for temporary workspace'
+  echo '  --dir-nimgcore <value>   top level directory where INC tools,'
+  echo '                           templates, etc. are stored,'
+  echo '                           default: ${DIR_NIMGCORE}'
+  echo '  --dir-pincsource <value> directory for PINC sourcefiles'
+  echo '                           default: ${DIR_PINCSOURCE}'
+  echo ''
+fi
+
+# Get time stamp for log -------------------------------------------------------
+proc_start=$(date +%Y-%m-%dT%H:%M:%S%z)
+
+#===============================================================================
+# Function Title
+#===============================================================================
+# <<body of function here>>
+# insert comments for important chunks
+
+# move files to appropriate locations
+
+#===============================================================================
+# End of Function
+#===============================================================================
+
+# Clean workspace --------------------------------------------------------------
+# edit directory for appropriate modality prep folder
+if [[ "${KEEP}" == "true" ]]; then
+  mkdir -p ${RESEARCHER}/${PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}
+  mv ${DIR_SCRATCH}/* ${RESEARCHER}/${PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}/
+  rmdir ${DIR_SCRATCH}
+else
+  rm ${DIR_SCRATCH}/*
+  rmdir ${DIR_SCRATCH}
+fi
+
+# Write log entry on conclusion ------------------------------------------------
+LOG_FILE=${RESEARCHER}/${PROJECT}/log/sub-${SUBJECT}_ses-${SESSION}.log
+date +"task:$0,start:"${proc_start}",end:%Y-%m-%dT%H:%M:%S%z" >> ${LOG_FILE}
+
