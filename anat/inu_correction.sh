2dc2073257398b9c5c8161fd1699ac4f43ada942
koscikt <timothy-koscik@uiowa.edu>
2020-02-27 13:11:09 -0600

    Update anat/inu_correction.sh, anat/brain_extraction.sh files

--- anat/inu_correction.sh
+++ anat/inu_correction.sh
@@ -9,7 +9,7 @@
 
 # Parse inputs -----------------------------------------------------------------
 OPTS=`getopt -o hv --long researcher:,project:,group:,subject:,session:,prefix:,\
-t1-image:,t2-image:,mask:,smooth-kernel:,\
+image:,method:,mask:,smooth-kernel:,\
 dir-save:,dir-scratch:,dir-nimgcore:,dir-pincsource:,\
 help,verbose -n 'parse-options' -- "$@"`
 if [ $? != 0 ]; then
@@ -25,8 +25,8 @@ GROUP=
 SUBJECT=
 SESSION=
 PREFIX=
-T1_IMAGE=
-T2_IMAGE=
+IMAGE=
+METHOD=
 MASK=
 SMOOTH_KERNEL=5
 DIR_SAVE=
@@ -46,8 +46,8 @@ while true; do
     --subject) SUBJECT="$2" ; shift 2 ;;
     --session) SESSION="$2" ; shift 2 ;;
     --prefix_ PREFIX="$2" ; shift 2 ;;
-    --t1-image) T1_IMAGE="$2" ; shift 2 ;;
-    --t2-image) T2_IMAGE="$2" ; shift 2 ;;
+    --image) IMAGE+=("$2") ; shift 2 ;;
+    --method) METHOD="$2" ; shift 2 ;;
     --mask) MASK="$2" ; shift 2 ;;
     --smooth-kernel) SMOOTH_KERNEL="$2" ; shift 2 ;;
     --dir-save) DIR_SAVE="$2" ; shift 2 ;;
@@ -65,8 +65,8 @@ if [[ "${HELP}" == "true" ]]; then
   echo ''
   echo '------------------------------------------------------------------------'
   echo "Iowa Neuroimage Processing Core: ${FUNC_NAME}"
-  echo 'Author: <<author names>>'
-  echo 'Date:   <<date of authorship>>'
+  echo 'Author: Timothy R. Koscik, PhD'
+  echo 'Date: 2020-02-25'
   echo '------------------------------------------------------------------------'
   echo "Usage: ${FUNC_NAME}"
   echo '  -h | --help              display command help'
@@ -78,8 +78,10 @@ if [[ "${HELP}" == "true" ]]; then
   echo '                           e.g., Research-kosciklab'
   echo '  --subject <value>        subject identifer, e.g., 123'
   echo '  --session <value>        session identifier, e.g., 1234abcd'
-  echo '  --t1-image <value>       full path to T1w image'
-  echo '  --t2-image <value>       full path to T2w image'
+  echo '  --image <value>          full path to image, multiple images allowed'
+  echo '                           if using T1T2, image 1 must be T1w, image 2'
+  echo '                           must be T2w'
+  echo '  --method <value>         one of N4 or T1T2 (case insensitive)'
   echo '  --mask <value>           full path to region mask'
   echo '  --smooth-kernel <value>  smoothing kernel size in mm, default: 5'
   echo '  --dir-save <value>       directory to save output,'
@@ -100,10 +102,8 @@ proc_start=$(date +%Y-%m-%dT%H:%M:%S%z)
 if [ -z "${DIR_SAVE}" ]; then
   DIR_SAVE=${RESEARCHER}/${PROJECT}/derivatives/anat/prep/sub-${SUBJECT}/ses-${SESSION}
 fi
-DIR_XFM==${RESEARCHER}/${PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}
 mkdir -r ${DIR_SCRATCH}
 mkdir -r ${DIR_SAVE}
-mkdir -r ${DIR_XFM}
 
 # set output prefix if not provided --------------------------------------------
 if [ -z "${PREFIX}" ]; then
@@ -113,77 +113,86 @@ fi
 # =============================================================================
 # Start of Function
 # =============================================================================
+if [[ "${METHOD,,}" == "t1t2" ]]; then
+  # Form sqrt(T1w*T2w), mask this and normalise by the mean
+  fslmaths ${IMAGE[0]} -mul ${IMAGE[1]} -abs -sqrt \
+    ${DIR_SCRATCH}/temp_t1mult2.nii.gz -odt float
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2.nii.gz -mas ${MASK} \
+    ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz
+  mean_brain_val=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz -M`
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz -div ${mean_brain_val} \
+    ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz
 
-t1out=$(echo "${t1img}" | grep -oP 'sub-\S+_')
-t2out=$(echo "${t2img}" | grep -oP 'sub-\S+_')
+  # Smooth the normalised sqrt image, within-mask smoothing: s(Mask*X)/s(Mask)
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz -bin -s ${SMOOTH_KERNEL} \
+    ${DIR_SCRATCH}/temp_smooth_norm.nii.gz
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz -s ${SMOOTH_KERNEL} \
+    -div ${DIR_SCRATCH}/temp_smooth_norm.nii.gz \
+    ${DIR_SCRATCH}/temp_t1mult2_brain_norm_s${SMOOTH_KERNEL}.nii.gz
 
-# Form sqrt(T1w*T2w), mask this and normalise by the mean ---------------------
-fslmaths ${T1_IMAGE} -mul ${T2_IMAGE} -abs -sqrt \
-  ${DIR_SCRATCH}/temp_t1mult2.nii.gz -odt float
+  # Divide normalised sqrt image by smoothed version
+  # (to do simple bias correction)
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz \
+    -div ${DIR_SCRATCH}/temp_t1mult2_brain_norm_s${SMOOTH_KERNEL}.nii.gz \
+    ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz
 
-fslmaths ${DIR_SCRATCH}/temp_t1mult2.nii.gz -mas ${MASK} \
-  ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz
+  # Create a mask using a threshold at Mean - 0.5*Stddev, with filling of holes
+  # to remove any non-grey/white tissue.
+  STD=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -S`
+  MEAN=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -M`
+  lower=`echo "${MEAN} - (${STD} * 0.5)" | bc -l`
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -thr ${lower} \
+    -bin -ero -mul 255 ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz
+  ${FSLDIR}/bin/cluster -i ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz \
+    -t 0.5 -o ${DIR_SCRATCH}/temp_cl_idx
+  MINMAX=`fslstats ${DIR_SCRATCH}/temp_cl_idx.nii.gz -R`
+  MAX=`echo "${MINMAX}" | cut -d ' ' -f 2`
+  fslmaths -dt int ${DIR_SCRATCH}/temp_cl_idx -thr ${MAX} -bin -mul 255 \
+    ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz
 
-mean_brain_val=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz -M`
+  # Extrapolate normalised sqrt image from mask region out to whole FOV
+  fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz \
+    -mas ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz -dilall \
+    ${DIR_SCRATCH}/temp_bias_raw.nii.gz -odt float
+  fslmaths ${DIR_SCRATCH}/temp_bias_raw.nii.gz -s ${smoothKernel} \
+    ${DIR_SCRATCH}/biasT1T2_Field.nii.gz
 
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain.nii.gz -div ${mean_brain_val} \
-  ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz
+  # Use bias field output to create corrected images
+  fslmaths ${T1_IMAGE} -div ${DIR_SCRATCH}/biasT1T2_Field.nii.gz \
+    ${DIR_SCRATCH}/biasT1T2_T1w.nii.gz
+  fslmaths ${T2_IMAGE} -div ${DIR_SCRATCH}/biasT1T2_Field.nii.gz \
+    ${DIR_SCRATCH}/biasT1T2_T2w.nii.gz
 
-# Smooth the normalised sqrt image, using within-mask smoothing : s(Mask*X)/s(Mask) -------------------
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz -bin -s ${SMOOTH_KERNEL} \
-  ${DIR_SCRATCH}/temp_smooth_norm.nii.gz
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz -s ${SMOOTH_KERNEL} \
-  -div ${DIR_SCRATCH}/temp_smooth_norm.nii.gz \
-  ${DIR_SCRATCH}/temp_t1mult2_brain_norm_s${SMOOTH_KERNEL}.nii.gz
+  # Move files to appropriate location
+  mv ${DIR_SCRATCH}/biasT1T2_T1w.nii.gz ${DIR_SAVE}/${PREFIX}_prep-bias+T1T2_T1w.nii.gz
+  mv ${DIR_SCRATCH}/biasT1T2_T2w.nii.gz ${DIR_SAVE}/${PREFIX}_prep-bias+T1T2_T2w.nii.gz
+  if [[ "${KEEP}" == "true" ]]; then
+    mv ${DIR_SCRATCH}/biasT1T2_Field.nii.gz ${DIR_SAVE}/${PREFIX}_prep-bias+T1T2+field.nii.gz
+  fi
+fi
+fi
 
-# Divide normalised sqrt image by smoothed version (to do simple bias correction) ---------------
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz \
-  -div ${DIR_SCRATCH}/temp_t1mult2_brain_norm_s${SMOOTH_KERNEL}.nii.gz \
-  ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz
+if [[ "${METHOD,,}" == "n4" ]]; then
+  NUM_IMAGE=${#IMAGE[@]}
+  for (( i=0; i<${NUM_IMAGE}; i++ )); then
+    # gather modality for output
+    MOD=(${IMAGE[${i}]})
+    MOD=(`basename "${MOD%.nii.gz}"`)
+    MOD=(${MOD##*_})
 
-# Create a mask using a threshold at Mean - 0.5*Stddev, with filling of holes to remove any non-grey/white tissue.
-STD=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -S`
-MEAN=`fslstats ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -M`
-lower=`echo "${MEAN} - (${STD} * 0.5)" | bc -l`
-
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod.nii.gz -thr ${lower} \
-  -bin -ero -mul 255 ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz
-
-${FSLDIR}/bin/cluster -i ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz \
-  -t 0.5 -o ${DIR_SCRATCH}/temp_cl_idx
-MINMAX=`fslstats ${DIR_SCRATCH}/temp_cl_idx.nii.gz -R`
-MAX=`echo "${MINMAX}" | cut -d ' ' -f 2`
-fslmaths -dt int ${DIR_SCRATCH}/temp_cl_idx -thr ${MAX} -bin -mul 255 \
-  ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz
-
-# Extrapolate normalised sqrt image from mask region out to whole FOV
-fslmaths ${DIR_SCRATCH}/temp_t1mult2_brain_norm.nii.gz \
-  -mas ${DIR_SCRATCH}/temp_t1mult2_brain_norm_mod_mask.nii.gz -dilall \
-  ${DIR_SCRATCH}/temp_bias_raw.nii.gz -odt float
-fslmaths ${DIR_SCRATCH}/temp_bias_raw.nii.gz -s ${smoothKernel} \
-  ${DIR_SCRATCH}/biasT1T2_Field.nii.gz
-
-# Use bias field output to create corrected images
-fslmaths ${T1_IMAGE} -div ${DIR_SCRATCH}/biasT1T2_Field.nii.gz \
-  ${DIR_SCRATCH}/biasT1T2_T1w.nii.gz
-fslmaths ${T2_IMAGE} -div ${DIR_SCRATCH}/biasT1T2_Field.nii.gz \
-  ${DIR_SCRATCH}/biasT1T2_T2w.nii.gz
-
-# Move files to appropriate location
-mv ${DIR_SCRATCH}/biasT1T2_T1w.nii.gz ${DIR_SAVE}/${PREFIX}_prep-biasT1T2_T1w.nii.gz
-mv ${DIR_SCRATCH}/biasT1T2_T2w.nii.gz ${DIR_SAVE}/${PREFIX}_prep-biasT1T2_T2w.nii.gz
+    N4BiasFieldCorrection -d 3 -r 1 -i ${IMAGE[${i}]} \
+      -o [${DIR_SCRATCH}/${PREFIX}_prep-bias+N4_${MOD}.nii.gz,${DIR_SCRATCH}/${PREFIX}_prep-bias+N4+field_${MOD}.nii.gz]
+    mv ${DIR_SCRATCH}/${PREFIX}_prep-bias+N4_${MOD}.nii.gz ${DIR_SAVE}/
+    if [[ "${KEEP}" == "true" ]]; then
+      mv ${DIR_SCRATCH}/${PREFIX}_prep-bias+N4+field_${MOD}.nii.gz ${DIR_SAVE}/
+    fi
+  done
+fi
 
 #===============================================================================
 # End of Function
 #===============================================================================
-
 # Clean workspace --------------------------------------------------------------
-# edit directory for appropriate modality prep folder
-if [[ "${KEEP}" == "true" ]]; then
-  mkdir -p ${RESEARCHER}/${PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}
-  mv ${DIR_SCRATCH}/biasT1T2_Field.nii.gz ${RESEARCHER}/${PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}/${PREFIX}_prep-biasT1T2+field.nii.gz
-fi
-rm ${DIR_SCRATCH}/*
 rmdir ${DIR_SCRATCH}
 
 # Write log entry on conclusion ------------------------------------------------
