0202ff4e8338b33c4367014d280913b8a7a2acd1
koscikt <timothy-koscik@uiowa.edu>
2020-03-10 12:24:25 -0500

    Update bids/summarize_3d.sh

--- bids/summarize_3d.sh
+++ bids/summarize_3d.sh
@@ -56,7 +56,7 @@ if [[ "${HELP}" == "true" ]]; then
   echo ''
   echo '------------------------------------------------------------------------'
   echo "Iowa Neuroimage Processing Core: ${FUNC_NAME}"
-  echo 'Author: <<author names>>'
+  echo 'Author: Timothy R. Koscik, PhD'
   echo 'Date:   <<date of authorship>>'
   echo '------------------------------------------------------------------------'
   echo "Usage: ${FUNC_NAME}"
@@ -69,17 +69,25 @@ if [[ "${HELP}" == "true" ]]; then
   echo '                           e.g., Research-kosciklab'
   echo '  --prefix <value>         scan prefix,'
   echo '                           default: sub-123_ses-1234abcd'
-  echo '  --other-inputs <value>   other inputs necessary for function'
-  echo '  --template <value>       name of template to use (if necessary),'
-  echo '                           e.g., HCPICBM'
-  echo '  --space <value>          spacing of template to use, e.g., 1mm'
-  echo '  --dir-save <value>       directory to save output, default varies by function'
+  echo '  --label <value>          file path to label to generate stats for.'
+  echo '                           Only one label file per function call allowed.'
+  echo '                           Label and Value files are assumed to be'
+  echo '                           registered, however they do not have to have'
+  echo '                           the same spacing. Label maps will be resampled'
+  echo '                           to match the spacing of value maps as needed'
+  echo '  --value <value>          file path to NIfTI file containing the values'
+  echo '                           to summarize, omit if only volumes are desired.'
+  echo '  --stats <value>          Image statistics to include in summary.'
+  echo '                           If value file not given (or if value and'
+  echo '                           label files are the same, only volumes will be given)'
+  echo '  --dir-save <value>       directory to save output,'
+  echo '                           default: DIR_PROJECT/summary'
   echo '  --dir-scratch <value>    directory for temporary workspace'
   echo '  --dir-nimgcore <value>   top level directory where INC tools,'
   echo '                           templates, etc. are stored,'
-  echo '                           default: ${DIR_NIMGCORE}'
+  echo "                           default: ${DIR_NIMGCORE}"
   echo '  --dir-pincsource <value> directory for PINC sourcefiles'
-  echo '                           default: ${DIR_PINCSOURCE}'
+  echo "                           default: ${DIR_PINCSOURCE}"
   echo ''
 fi
 
@@ -98,6 +106,7 @@ mkdir -p ${DIR_SAVE}
 #===============================================================================
 if [[ -z ${VALUE} ]]; then
   STATS=("volume")
+  VALUE=${LABEL}
 fi
 NUM_VALUE=${#VALUE[@]}
 NUM_STATS=${#STATS[@]}
@@ -108,18 +117,18 @@ LUT=${DIR_NIMGCORE}/code/lut/lut-${LABEL_NAME}.csv
 while IFS=$',\r' read -r a b c;
 do
   LABEL_VALUE+=(${a})
-  LABEL_TEXT+=(${b})
-  LABEL_ACRONYM+=(${c})
+  LABEL_ACRONYM+=(${b})
+  LABEL_TEXT+=(${c})
 done < ${LUT}
 NUM_LABEL=${#LABEL_VALUE[@]}
 
 LABEL_ORIG=${LABEL}
 for (( i=0; i<${NUM_VALUE}; i++ )); do
-  if [[ -z ${VALUE} ]]; do
+  if [[ "${VALUE[${i}]}" == "${LABEL}" ]]; do
     # Assuming subject specific labels
     SUBJECT=(`${DIR_NIMGCORE}/code/bids/get_field.sh -i ${LABEL} -f "sub"`)
     SESSION=(`${DIR_NIMGCORE}/code/bids/get_field.sh -i ${LABEL} -f "ses"`)
-    IFS=x read -r -a pixdim <<< $(PrintHeader ${LABEL} 1)
+    MOD="volume"
   else
     # Assuming labels apply to all inputs,
     # inputs must be coregistered, but not necessarily the same spacing
@@ -136,6 +145,7 @@ for (( i=0; i<${NUM_VALUE}; i++ )); do
     else
       LABEL=${LABEL_ORIG}
     fi
+    MOD=`${DIR_NIMGCORE}/code/bids/get_field.sh -i ${VALUE[${i}]} -f "modality"`
   fi
   
   # initialize temporary file for output
@@ -165,40 +175,57 @@ for (( i=0; i<${NUM_VALUE}; i++ )); do
     fslmaths ${LABEL} -thr ${LABEL_VALUE[${j}]} -uthr ${LABEL_VALUE[${j}]} -bin ${temp_mask}
     for (( k=0; k<${NUM_STATS}; k++ )); do
       if [[ "${STATS[${k}]},," == "volume" ]]; then
-        volume=`fslstats ${VALUE[${i}]} -k ${temp_mask} -v`
-        volume=`echo "${volume}*${pixdim[0]}*${pixdim[1]}*${pixdim[2]}" | bc -l`
-        echo ${volume} >> ${DIR_SCRATCH}/temp.txt
+        volume=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -v`)
+        echo ${volume[1]} >> ${DIR_SCRATCH}/temp.txt
       fi
-      ### Restart here
       if [[ "${STATS[${k}]},," == "mean" ]]; then
-        temp=`fslstats ${VALUE[${i}]} -k ${temp_mask} -m`
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -m`)
         echo ${temp} >> ${DIR_SCRATCH}/temp.txt
       fi
       if [[ "${STATS[${k}]},," == "std" ]]; then
-        temp=`fslstats ${VALUE[${i}]} -k ${temp_mask} -s`
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -s`)
         echo ${temp} >> ${DIR_SCRATCH}/temp.txt
       fi
       if [[ "${STATS[${k}]},," == "cog" ]]; then
-        temp=`fslstats ${VALUE[${i}]} -k ${temp_mask} -c`
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -c`)
+        echo "(${temp[0]} ${temp[1]} ${temp[2]})" >> ${DIR_SCRATCH}/temp.txt
+      fi
+      last_char=${STATS[${k}]: -1}
+      if [[ "${STATS[${k}]: -1}" == "%"  ]]; then
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -p ${STATS[${k}]::-1}`)
         echo ${temp} >> ${DIR_SCRATCH}/temp.txt
       fi
-      last_char=
-      if [[ "${STATS[${k}]},," == "" ]]; then
-        temp=`fslstats ${VALUE[${i}]} -k ${temp_mask} -`
+      if [[ "${STATS[${k}]},," == "min" ]]; then
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -R`)
+        echo ${temp[0]} >> ${DIR_SCRATCH}/temp.txt
+      fi
+      if [[ "${STATS[${k}]},," == "max" ]]; then
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -R`)
+        echo ${temp[1]} >> ${DIR_SCRATCH}/temp.txt
+      fi
+      if [[ "${STATS[${k}]},," == "entropy" ]]; then
+        temp=(`fslstats ${VALUE[${i}]} -k ${temp_mask} -e`)
         echo ${temp} >> ${DIR_SCRATCH}/temp.txt
       fi
     done
+    paste -d , ${OUTPUT} ${DIR_SCRATCH}/temp.txt > ${DIR_SCRATCH}/temp_cat.txt
+    mv ${DIR_SCRATCH}/temp_cat.txt ${OUTPUT}
+    rm ${DIR_SCRATCH}/temp.txt
   done
+
+  # write output to summary file
+  mkdir -p ${DIR_PROJECT}/summary
+  PROJECT=`${DIR_NIMGCORE}/code/bids/get_project.sh -i `
+  if 
+  SUMMARY_FILE=${DIR_PROJECT}/summary/${PROJECT}_${MOD}_label-${LABEL_NAME}.csv
+  if [[ ! -f ${SUMMARY_FILE} ]]; then
+    LABEL_TEMP="${LABEL_ACRONYM[@]:1}"
+    HEADER=("participant_id" "session_id" "summary_date" "measure" "${LABEL_TEMP}")
+    HEADER="${HEADER[@]}"
+    echo ${HEADER// /,} >> ${SUMMARY_FILE}
+  fi
+  cat ${OUTPUT} >> ${SUMMARY_FILE}
 done
-
-  ## get just volumes--------------
-  # get subject and session identifiers
-  # get label identifier, identify target file (if does not exist initialize with lookup table headers)
-  # get voxel dimensions
-  # loop over label files
-  ## split labels into volumes
-  ## fslstats input -k label_mask -v
-
 
 
 #===============================================================================
@@ -206,15 +233,8 @@ done
 #===============================================================================
 
 # Clean workspace --------------------------------------------------------------
-# edit directory for appropriate modality prep folder
-if [[ "${KEEP}" == "true" ]]; then
-  mkdir -p ${DIR_PROJECT}/derivatives/anat/prep/sub-${SUBJECT}/ses-${SESSION}
-  mv ${DIR_SCRATCH}/* ${DIR_PROJECT}/derivatives/anat/prep/sub-${SUBJECT}/ses-${SESSION}/
-  rmdir ${DIR_SCRATCH}
-else
-  rm ${DIR_SCRATCH}/*
-  rmdir ${DIR_SCRATCH}
-fi
+rm ${DIR_SCRATCH}/*
+rmdir ${DIR_SCRATCH}
 
 # Write log entry on conclusion ------------------------------------------------
 if [[ "${NO_LOG}" == "false" ]]; then
