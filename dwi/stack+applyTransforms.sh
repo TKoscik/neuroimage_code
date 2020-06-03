f544f23953fdc703abc9e592f965387fa32d3c84
sjcochran <steven-j-cochran@uiowa.edu>
2020-06-03 13:28:38 -0500

    Update dwi/stack+applyTransforms.sh

--- dwi/stack+applyTransforms.sh
+++ dwi/stack+applyTransforms.sh
@@ -122,7 +122,7 @@ unset xfm
 REF_IMAGE=${DIR_TEMPLATE}/${SPACE}/${TEMPLATE}/${SPACE}_${TEMPLATE}_T1w.nii.gz
 antsApplyTransforms -d 3 \
   -o [${DIR_XFM}/${PREFIX}_from-dwi+b0_to-${SPACE}+${TEMPLATE}_xfm-stack.nii.gz,1] \
-  -t ${DIR_XFM}/${PREFIX}_from-T1w+rigid_to-${SPACE}+${TEMPLATE}_xfm-stack.nii.gz \
+  -t ${DIR_XFM}/${PREFIX}_from-native_to-${SPACE}+${TEMPLATE}_xfm-stack.nii.gz \
   -t ${DIR_XFM}/${PREFIX}_from-dwi+b0_to-T2w+rigid_xfm-syn.nii.gz \
   -t ${DIR_XFM}/${PREFIX}_from-dwi+b0_to-T2w+rigid_xfm-affine.mat \
   -r ${REF_IMAGE}
