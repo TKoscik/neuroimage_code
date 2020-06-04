58092f118a993b14b658d3fea2d734a3fe9527f2
sjcochran <steven-j-cochran@uiowa.edu>
2020-06-04 16:15:30 -0500

    Update qc/qc_dicom_conversion.R

--- qc/qc_dicom_conversion.R
+++ qc/qc_dicom_conversion.R
@@ -1,9 +1,8 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-researcher <- args[1]
-project <- args[2]
-dir.input <- args[3]
-dir.inc.root <- "/Shared/nopoulos/nimg_core"
+dir.project <- args[1]
+dir.input <- args[2]
+dir.inc.root <- "/Shared/inc_scratch/code"
 if (length(args) > 4 ) {
   for (i in seq(4, length(args), 2)) {
     if (args[i] == "dir.inc.root") {
@@ -13,14 +12,14 @@ if (length(args) > 4 ) {
 }
 
 ## debug ----
-# dir.input <- "/Shared/koscikt/temp/scratch/nifti"
+# dir.input <- "/Shared/koscikt/temp/scratch/rawdata"
 # dir.inc.root <- "/Shared/nopoulos/nimg_core"
 # researcher <- "/Shared/koscikt"
 # project <- "temp"
 ## ----
 
 library(tools)
-source(paste0(dir.inc.root, "/inc_ses_decode.R"))
+source(paste0(dir.inc.root, "dicom/inc_ses_decode.R"))
 
 fls <- list.files(dir.input, pattern="sub-")
 fls <- fls[grepl(fls, pattern="nii")]
@@ -75,35 +74,35 @@ write('#### Iowa Neuroimage Processing Core, `r format(Sys.time(), "%Y-%m-%dT%H:
 write("", rmd.fid, sep="\n", append=TRUE)
 write("***", rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
-write(paste0("Parent Directory: __", researcher, "/", project, "__  "), rmd.fid, sep="\n", append=TRUE)
+write(paste0("Parent Directory: __", dir.project, "__  "), rmd.fid, sep="\n", append=TRUE)
 write(paste0("Subject: __", subject, "__  "), rmd.fid, sep="\n", append=TRUE)
 write(paste0("Session: __", session, "__  "), rmd.fid, sep="\n", append=TRUE)
 write(paste0("Date of Scan: __", dot, "__  "), rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
 write("***", rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
-write(paste0(researcher, "/", project, "/nifti/sub-", subject, "/ses-", session, "/  "), rmd.fid, sep="\n", append=TRUE)
+write(paste0(dir.project, "/rawdata/sub-", subject, "/ses-", session, "/  "), rmd.fid, sep="\n", append=TRUE)
 write("```{bash}", rmd.fid, sep="\n", append=TRUE)
-write(paste0("tree -f ", researcher, "/", project, "/nifti/sub-", subject, "/ses-", session, " \\"), rmd.fid, sep="\n", append=TRUE)
-write(paste0("  -H ", dir.input, "/", prefix, "_nifti-tree.html \\"), rmd.fid, sep="\n", append=TRUE)
-write(paste0("  -o ", dir.input, "/", prefix, "_nifti-tree.html \\"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("tree -f ", dir.project, "/rawdata/sub-", subject, "/ses-", session, " \\"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("  -H ", dir.input, "/", prefix, "_rawdata-tree.html \\"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("  -o ", dir.input, "/", prefix, "_rawdata-tree.html \\"), rmd.fid, sep="\n", append=TRUE)
 write('  --nolinks --noreport', rmd.fid, sep="\n", append=TRUE)
-write(paste0("sed -i \'31d\' ", dir.input, "/", prefix, "_nifti-tree.html"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("sed -i \'31d\' ", dir.input, "/", prefix, "_rawdata-tree.html"), rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
 write("for i in {1..13}; do", rmd.fid, sep="\n", append=TRUE)
-write(paste0("  sed -i \'$d\' ", dir.input, "/", prefix, "_nifti-tree.html"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("  sed -i \'$d\' ", dir.input, "/", prefix, "_rawdata-tree.html"), rmd.fid, sep="\n", append=TRUE)
 write("done", rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
-write(paste0("echo \"</body>\" >> ", dir.input, "/", prefix, "_nifti-tree.html"), rmd.fid, sep="\n", append=TRUE)
-write(paste0("echo \"</html>\" >> ", dir.input, "/", prefix, "_nifti-tree.html"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("echo \"</body>\" >> ", dir.input, "/", prefix, "_rawdata-tree.html"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("echo \"</html>\" >> ", dir.input, "/", prefix, "_rawdata-tree.html"), rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
 write("```", rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
 write("```{r}", rmd.fid, sep="\n", append=TRUE)
-write(paste0("x <- readLines(\"", dir.input, "/", prefix, "_nifti-tree.html\")"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("x <- readLines(\"", dir.input, "/", prefix, "_rawdata-tree.html\")"), rmd.fid, sep="\n", append=TRUE)
 write("x <- x[-which(x==\"\\t<h1>Directory Tree</h1><p>\")]", rmd.fid, sep="\n", append=TRUE)
-write(paste0("writeLines(x, con=\"", dir.input, "/", prefix, "_nifti-tree.html\", sep = \"\\n\")"), rmd.fid, sep="\n", append=TRUE)
-write(paste0("includeHTML(\"", dir.input, "/", prefix, "_nifti-tree.html\")"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("writeLines(x, con=\"", dir.input, "/", prefix, "_rawdata-tree.html\", sep = \"\\n\")"), rmd.fid, sep="\n", append=TRUE)
+write(paste0("includeHTML(\"", dir.input, "/", prefix, "_rawdata-tree.html\")"), rmd.fid, sep="\n", append=TRUE)
 write("```", rmd.fid, sep="\n", append=TRUE)
 write("", rmd.fid, sep="\n", append=TRUE)
 write("***", rmd.fid, sep="\n", append=TRUE)
