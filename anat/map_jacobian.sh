9e12e6022660dc563c0f8daad3d09b87ebf0ec08
koscikt <timothy-koscik@uiowa.edu>
2020-09-02 08:55:33 -0500

    Update anat/map_jacobian.sh

--- anat/map_jacobian.sh
+++ anat/map_jacobian.sh
@@ -162,14 +162,16 @@ fi
 mkdir -p ${DIR_SAVE}
 
 # create temporary stack of transforms
-xfm_fcn="antsApplyTransforms"
-xfm_fcn="${xfm_fcn} -d 3 -v ${VERBOSE}"
-xfm_fcn="${xfm_fcn} -o [${DIR_SCRATCH}/${PREFIX}_from-${FROM}_to-${TO}_xfm-stack.nii.gz,1]"
-xfm_fcn="${xfm_fcn} -n ${INTERPOLATION}"
-for (( i=0; i<${N_XFM}; i++ )); do
-  xfm_fcn="${xfm_fcn} -t ${XFM[$[{i}]}"
-done
-eval ${xfm_fcn}
+if [[ "${N_XFM}" > 1 ]]; then
+  xfm_fcn="antsApplyTransforms"
+  xfm_fcn="${xfm_fcn} -d 3 -v ${VERBOSE}"
+  xfm_fcn="${xfm_fcn} -o [${DIR_SCRATCH}/${PREFIX}_from-${FROM}_to-${TO}_xfm-stack.nii.gz,1]"
+  xfm_fcn="${xfm_fcn} -n ${INTERPOLATION}"
+  for (( i=0; i<${N_XFM}; i++ )); do
+    xfm_fcn="${xfm_fcn} -t ${XFM[$[{i}]}"
+  done
+  eval ${xfm_fcn}
+fi
 
 # Create Jacobian Determinant imgae
 CreateJacobianDeterminantImage 3 \
