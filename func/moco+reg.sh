376181bab9d42e9e8747a380c3bef86df444ed18
koscikt <timothy-koscik@uiowa.edu>
2020-10-22 08:59:33 -0500

    Update func/moco+reg.sh

--- func/moco+reg.sh
+++ func/moco+reg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 PROC_START=$(date +%Y-%m-%dT%H:%M:%S%z)
-FCN_NAME=(`basename "$0"`)
+FCN_NAME=($(basename "$0"))
 DATE_SUFFIX=$(date +%Y%m%dT%H%M%S%N)
 OPERATOR=$(whoami)
 KEEP=false
@@ -39,13 +39,9 @@ NO_LOG=false
 # TODO: Add QC function or source QC script
 #===============================================================================
 
-#userID=`whoami`
-set -u
-
 # actions on exit, write to logs, clean scratch
 function egress {
   EXIT_CODE=$?
-  #if [[ "${DEBUG}" = false ]]; then
   if [[ "${KEEP}" = false ]]; then
     if [[ -n "${DIR_SCRATCH}" ]]; then
       if [[ -d "${DIR_SCRATCH}" ]]; then
@@ -57,7 +53,7 @@ function egress {
       fi
     fi
   fi
-  LOG_STRING=`date +"${OPERATOR}\t${FCN_NAME}\t${PROC_START}\t%Y-%m-%dT%H:%M:%S%z\t${EXIT_CODE}"`
+  LOG_STRING=$(date +"${OPERATOR}\t${FCN_NAME}\t${PROC_START}\t%Y-%m-%dT%H:%M:%S%z\t${EXIT_CODE}")
   if [[ "${NO_LOG}" = false ]]; then
     FCN_LOG=/Shared/inc_scratch/log/benchmark_${FCN_NAME}.log
     if [[ ! -f ${FCN_LOG} ]]; then
@@ -79,7 +75,7 @@ trap egress EXIT
 # Parse inputs -----------------------------------------------------------------
 OPTS=`getopt -o hvkl --long prefix:,\
 ts-bold:,target:,template:,space:,\
-dir-save:,dir-scratch:,dir-code:,dir-template:,dir-pincsource:,\
+dir-save:,dir-scratch:,\
 keep,help,verbose,no-log -n 'parse-options' -- "$@"`
 if [ $? != 0 ]; then
   echo "Failed parsing options" >&2
@@ -96,16 +92,12 @@ TEMPLATE=
 SPACE=
 DIR_SAVE=
 DIR_SCRATCH=/Shared/inc_scratch/scratch_${OPERATOR}_${DATE_SUFFIX}
-#For testing below
-#DIR_SCRATCH=~
 DIR_CODE=/Shared/inc_scratch/code
 DIR_TEMPLATE=/Shared/nopoulos/nimg_core/templates_human
 DIR_PINCSOURCE=/Shared/pinc/sharedopt/apps/sourcefiles
 HELP=false
 VERBOSE=1
 KEEP=false
-NO_LOG=false
-IS_SES=true
 
 while true; do
   case "$1" in
@@ -120,9 +112,6 @@ while true; do
     --space) SPACE="$2" ; shift 2 ;;
     --dir-save) DIR_SAVE="$2" ; shift 2 ;;
     --dir-scratch) DIR_SCRATCH="$2" ; shift 2 ;;
-    --dir-code) DIR_CODE="$2" ; shift 2 ;;
-    --dir-template) DIR_TEMPLATE="$2" ; shift 2 ;;
-    --dir-pincsource) DIR_PINCSOURCE="$2" ; shift 2 ;;
     -- ) shift ; break ;;
     * ) break ;;
   esac
@@ -130,12 +119,11 @@ done
 
 # Usage Help -------------------------------------------------------------------
 if [[ "${HELP}" == "true" ]]; then
-  FUNC_NAME=(`basename "$0"`)
   echo ''
   echo '------------------------------------------------------------------------'
-  echo "Iowa Neuroimage Processing Core: ${FUNC_NAME}"
+  echo "Iowa Neuroimage Processing Core: ${FCN_NAME}"
   echo '------------------------------------------------------------------------'
-  echo "Usage: ${FUNC_NAME}"
+  echo "Usage: ${FCN_NAME}"
   echo '  -h | --help              display command help'
   echo '  -v | --verbose           add verbose output to log file'
   echo '  -k | --keep              keep preliminary processing steps'
@@ -148,41 +136,26 @@ if [[ "${HELP}" == "true" ]]; then
   echo '  --space <value>          spacing of template to use, e.g., 1mm'
   echo '  --dir-save <value>       directory to save output, default varies by function'
   echo '  --dir-scratch <value>    directory for temporary workspace'
-  echo '  --dir-code <value>       directory where INC tools are stored,'
-  echo '                           default: ${DIR_CODE}'
-  echo '  --dir-template <value>   directory where INC templates are stored,'
-  echo '                           default: ${DIR_TEMPLATE}'
-  echo '  --dir-pincsource <value> directory for PINC sourcefiles'
-  echo '                           default: ${DIR_PINCSOURCE}'
   echo ''
   NO_LOG=true
   exit 0
 fi
 
-#Debugging statement - only uncomment if debugging
-#read -p "Press [Enter] key to continue debugging..."
+#==============================================================================
+# Start of Function
+#==============================================================================
 
 # Set up BIDs compliant variables and workspace --------------------------------
-proc_start=$(date +%Y-%m-%dT%H:%M:%S%z)
-
 if [ -f "${TS_BOLD}" ]; then
   DIR_PROJECT=`${DIR_CODE}/bids/get_dir.sh -i ${TS_BOLD}`
   SUBJECT=`${DIR_CODE}/bids/get_field.sh -i ${TS_BOLD} -f "sub"`
   SESSION=`${DIR_CODE}/bids/get_field.sh -i ${TS_BOLD} -f "ses"`
-  TASK=`${DIR_CODE}/bids/get_field.sh -i ${TS_BOLD} -f "task"`
-  RUN=`${DIR_CODE}/bids/get_field.sh -i ${TS_BOLD} -f "run"`
   if [ -z "${PREFIX}" ]; then
     PREFIX=`${DIR_CODE}/bids/get_bidsbase -s -i ${TS_BOLD}}`
   fi
 else
   echo "The BOLD file does not exist. Exiting."
-  echo "Check paths, file names, and arguments."
   exit 1
-fi
-
-# Set IS_SES variable
-if [ -z "${SESSION}" ]; then
-  IS_SES=false
 fi
 
 # Set DIR_SAVE variable
@@ -192,24 +165,79 @@ fi
 mkdir -p ${DIR_SCRATCH}
 mkdir -p ${DIR_SAVE}
 
-#==============================================================================
-# Motion Correction + registration
-#==============================================================================
+# Check if required files exist -----------------------------------------------
+# Set some helper variables depending on whether session is specified
+if [[ -n "${SESSION}" ]]; then
+  DIR_SUBSES=sub-${SUBJECT}/ses-${SESSION}
+  SUBSES=sub-${SUBJECT}_ses-${SESSION}
+else
+  DIR_SUBSES=sub-${SUBJECT}
+  SUBSES=sub-${SUBJECT}
+fi
 
+# Native anatomical, brain mask, and rigid alignment transform
+ANAT=($(ls ${DIR_PROJECT}/derivatives/anat/native/${SUBSES}*${TARGET}.nii.gz))
+ANAT_MASK=($(ls ${DIR_PROJECT}/derivatives/anat/mask/${SUBSES}*mask-brain*.nii.gz))
+XFM_ALIGN=($(ls ${DIR_PROJECT}/derivatives/xfm/${DIR_SUBSES}/${SUBSES}*from-${TARGET}+raw_to-${TEMPLATE}*.mat))
+if [[ -z ${ANAT} ]]; then
+  echo "Native anatomical not found, aborting."
+  exit 1
+fi
+if [[ -z ${ANAT_MASK} ]]; then
+  echo "Native anatomical mask not found, aborting."
+  exit 1
+fi
+if [[ -n ${XFM_ALIGN} ]]; then
+  INIT_XFM=${XFM_ALIGN}
+else
+  echo "Alignment registration not found, continuing."
+  INIT_XFM=[${ANAT},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1]
+  #  exit 1
+fi
+
+# Find transforms
+DIR_XFM=${DIR_PROJECT}/derivatives/xfm/${DIR_SUBSES}
+unset XFM_LS XFM_ARG XFM_NORM
+XFM_LS=($(ls ${DIR_XFM}/*native_to-${TEMPLATE}* 2>/dev/null))
+if [[ -z ${XFM_LS} ]]; then
+  # note: the line below is for backward compatibility with slightly different naming scheme
+  XFM_LS=($(ls ${DIR_XFM}/*${TARGET}+rigid_to-${TEMPLATE}* 2>/dev/null))
+  if [[ -z ${XFM_LS} ]]; then
+    echo "Normalization transform(s) not found, aborting"
+    exit 1
+  fi
+fi
+
+unset XFM_STACK XFM_RIGID XFM_AFFINE XFM_SYN
+for (( i=0; i<${#XFM_LS[@]}; i++ )); do
+  unset XFM_ARG
+  XFM_ARG=$(${DIR_CODE}/bids/get_field.sh -i ${XFM_LS[${i}]} -f xfm)
+  if [[ "${XFM_ARG,,}" == "rigid" ]]; then XFM_RIGID=${XFM_LS[${i}]}; fi
+  if [[ "${XFM_ARG,,}" == "affine" ]]; then XFM_AFFINE=${XFM_LS[${i}]}; fi
+  if [[ "${XFM_ARG,,}" == "syn" ]]; then XFM_SYN=${XFM_LS[${i}]}; fi
+  if [[ "${XFM_ARG,,}" == "stack" ]]; then XFM_STACK=${XFM_LS[${i}]}; fi
+done
+if [[ -n ${XFM_STACK} ]]; then 
+  XFM_NORM=${XFM_STACK}
+else
+  if [[ -n ${XFM_SYN} ]]; then XFM_NORM+=(${XFM_SYN}); fi
+  if [[ -n ${XFM_AFFINE} ]]; then XFM_NORM+=(${XFM_AFFINE}); fi
+  if [[ -n ${XFM_RIGID} ]]; then XFM_NORM+=(${XFM_RIGID}); fi
+fi
+N_XFM=${#XFM_NORM[@]}
+
+# Motion Correction + registration ============================================
 # Get timeseries info ---------------------------------------------------------
 NUM_TR=`PrintHeader ${TS_BOLD} | grep Dimens | cut -d ',' -f 4 | cut -d ']' -f 1`
 TR=`PrintHeader ${TS_BOLD} | grep "Voxel Spac" | cut -d ',' -f 4 | cut -d ']' -f 1`
-
-### put check in here for 4d file.
-### L. Hopkins 7/2/2020
+# check in here for 4d file.
 if [ "${NUM_TR}" == 1 ]; then
     echo "Input file is not a 4D file. Aborting."
-    exit
+    exit 1
 fi
 
-
 # Motion correction -----------------------------------------------------------
-# pad image
+# pad image for better registration
 fslsplit ${TS_BOLD} ${DIR_SCRATCH}/vol -t
 for (( i=0; i<${NUM_TR}; i++ )); do
   VOL_NUM=$(printf "%04d" ${i})
@@ -249,87 +277,56 @@ rm ${DIR_SCRATCH}/${PREFIX}_affine_MOCOparams.csv
 
 # get brain mask of mean BOLD -------------------------------------------------
 bet ${DIR_SCRATCH}/${PREFIX}_avg.nii.gz ${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz -m -n
-#read -p "Press [Enter] key to continue debugging..."
-
 mv ${DIR_SCRATCH}/${PREFIX}_mask-brain_mask.nii.gz ${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz
 
 # Registration to subject space -----------------------------------------------
-# L. Hopkins 7/22/2020 - Add options for situations where there is no SESSION info
-if [ "${IS_SES}" = true ]; then
-  T1=(`ls ${DIR_PROJECT}/derivatives/anat/native/sub-${SUBJECT}_ses-${SESSION}*${TARGET}.nii.gz`)
-  T1_MASK=(`ls ${DIR_PROJECT}/derivatives/anat/mask/sub-${SUBJECT}_ses-${SESSION}*mask-brain*.nii.gz`)
-  # use raw to rigid transform to initialize, if it exists
-  RAW_TO_RIGID=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}/sub-${SUBJECT}_ses-${SESSION}*${TARGET}+raw*${TEMPLATE}+${SPACE}*.mat`)
-else
-  T1=(`ls ${DIR_PROJECT}/derivatives/anat/native/sub-${SUBJECT}*${TARGET}.nii.gz`)
-  T1_MASK=(`ls ${DIR_PROJECT}/derivatives/anat/mask/sub-${SUBJECT}*mask-brain*.nii.gz`)
-  RAW_TO_RIGID=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/sub-${SUBJECT}*${TARGET}+raw*${TEMPLATE}+${SPACE}*.mat`)
-fi
-
-if [ ! -z "${RAW_TO_RIGID}" ]; then
-  INIT_XFM=${RAW_TO_RIGID}
-else
-  INIT_XFM=[${T1},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1]
-fi
-
 antsRegistration \
   -d 3 -u 0 -z 1 -l 1 -n Linear -v ${VERBOSE} \
   -o ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_ \
   -r ${INIT_XFM} \
   -t Rigid[0.25] \
-  -m Mattes[${T1},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32,Regular,0.2] \
+  -m Mattes[${ANAT},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32,Regular,0.2] \
   -c [1200x1200x100,1e-6,5] -f 4x2x1 -s 2x1x0vox \
-  -x [${T1_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz] \
+  -x [${ANAT_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz] \
   -t Affine[0.25] \
-  -m Mattes[${T1},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32,Regular,0.2] \
+  -m Mattes[${ANAT},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32,Regular,0.2] \
   -c [200x20,1e-6,5] -f 2x1 -s 1x0vox \
-  -x [${T1_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz] \
+  -x [${ANAT_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz] \
   -t SyN[0.2,3,0] \
-  -m Mattes[${T1},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32] \
+  -m Mattes[${ANAT},${DIR_SCRATCH}/${PREFIX}_avg.nii.gz,1,32] \
   -c [40x20x0,1e-7,8] -f 4x2x1 -s 2x1x0vox \
-  -x [${T1_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz]
+  -x [${ANAT_MASK},${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz]
 
 # collapse transforms to deformation field
 antsApplyTransforms -d 3 \
   -o [${DIR_SCRATCH}/${PREFIX}_xfm_toNative_stack.nii.gz,1] \
   -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_1Warp.nii.gz \
   -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_0GenericAffine.mat \
-  -r ${T1}
+  -r ${ANAT}
 
 # Push mean bold to template --------------------------------------------------
-if [ "${IS_SES}" = true ]; then
-  if [ -f ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}/*${TARGET}+rigid_to-${TEMPLATE}+*_xfm-stack.nii.gz ]; then
-    XFM_NORM=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}/*${TARGET}+rigid_to-${TEMPLATE}+*_xfm-stack.nii.gz`)
-  elif [ -f ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}/sub-${SUBJECT}_ses-${SESSION}_from-native_to-${TEMPLATE}+${SPACE}_xfm-stack.nii.gz ]; then
-    XFM_NORM=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/ses-${SESSION}/sub-${SUBJECT}_ses-${SESSION}_from-native_to-${TEMPLATE}+${SPACE}_xfm-stack.nii.gz`)
-  else
-    echo "${DIR_PROJECT}/derivatives/xfm/ doesn't have the right stack."
-    exit 1
-  fi
-else
-  if [ -f ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/*${TARGET}+rigid_to-${TEMPLATE}+*_xfm-stack.nii.gz ]; then
-    XFM_NORM=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/*${TARGET}+rigid_to-${TEMPLATE}+*_xfm-stack.nii.gz`)
-  elif [ -f ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/sub-${SUBJECT}_from-native_to-${TEMPLATE}+${SPACE}_xfm-stack.nii.gz ]; then
-    XFM_NORM=(`ls ${DIR_PROJECT}/derivatives/xfm/sub-${SUBJECT}/sub-${SUBJECT}_from-native_to-${TEMPLATE}+${SPACE}_xfm-stack.nii.gz`)
-  else
-    echo "${DIR_PROJECT}/derivatives/xfm/ doesn't have the right stack."
-    exit 1
-  fi
-fi
+unset xfm_fcn
+xfn_fcn="antsApplyTransforms -d 3"
+xfm_fcn="${xfm_fcn} -o ${DIR_SCRATCH}/${PREFIX}_avg+warp.nii.gz"
+xfm_fcn="${xfm_fcn} -i ${DIR_SCRATCH}/${PREFIX}_avg.nii.gz"
+for (( i=0; i<${N_XFM}; i++ )); do
+  xfm_fcn="${xfm_fcn} ${XFM_NORM}"
+done
+xfm_fcn="${xfm_fcn} -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_stack.nii.gz"
+xfm_fcn="${xfm_fcn} -r ${DIR_TEMPLATE}/${TEMPLATE}/${SPACE}/${TEMPLATE}_${SPACE}_T1w.nii.gz"
+eval ${xfm_fcn}
 
-antsApplyTransforms -d 3 \
-  -o ${DIR_SCRATCH}/${PREFIX}_avg+warp.nii.gz \
-  -i ${DIR_SCRATCH}/${PREFIX}_avg.nii.gz \
-  -t ${XFM_NORM} \
-  -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_stack.nii.gz \
-  -r ${DIR_TEMPLATE}/${TEMPLATE}/${SPACE}/${TEMPLATE}_${SPACE}_T1w.nii.gz
 # apply to brain mask as well
-antsApplyTransforms -d 3 -n NearestNeighbor\
-  -o ${DIR_SCRATCH}/${PREFIX}_mask-brain+warp.nii.gz \
-  -i ${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz \
-  -t ${XFM_NORM} \
-  -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_stack.nii.gz \
-  -r ${DIR_TEMPLATE}/${TEMPLATE}/${SPACE}/${TEMPLATE}_${SPACE}_T1w.nii.gz
+unset xfm_fcn
+xfn_fcn="antsApplyTransforms -d 3 -n NearestNeighbor"
+xfm_fcn="${xfm_fcn} -o ${DIR_SCRATCH}/${PREFIX}_mask-brain+warp.nii.gz"
+xfm_fcn="${xfm_fcn} -i ${DIR_SCRATCH}/${PREFIX}_mask-brain.nii.gz"
+for (( i=0; i<${N_XFM}; i++ )); do
+  xfm_fcn="${xfm_fcn} ${XFM_NORM}"
+done
+xfm_fcn="${xfm_fcn} -t ${DIR_SCRATCH}/${PREFIX}_xfm_toNative_stack.nii.gz"
+xfm_fcn="${xfm_fcn} -r ${DIR_TEMPLATE}/${TEMPLATE}/${SPACE}/${TEMPLATE}_${SPACE}_T1w.nii.gz"
+eval ${xfm_fcn}
 
 # redo motion correction to normalized mean bold ------------------------------
 antsMotionCorr \
@@ -353,11 +350,7 @@ fslmerge -tr ${DIR_SCRATCH}/${PREFIX}_bold.nii.gz ${MERGE_LS[@]} ${TR}
 rm ${MERGE_LS[@]}
 
 # Move files to appropriate locations -----------------------------------------
-if [ "${IS_SES}" = true ]; then
-  DIR_REGRESSOR=${DIR_PROJECT}/derivatives/func/regressors/sub-${SUBJECT}/ses-${SESSION}
-else
-  DIR_REGRESSOR=${DIR_PROJECT}/derivatives/func/regressors/sub-${SUBJECT}
-fi
+DIR_REGRESSOR=${DIR_PROJECT}/derivatives/func/regressors/${DIR_SUBSES}
 mkdir -p ${DIR_REGRESSOR}
 mv ${DIR_SCRATCH}/${PREFIX}_moco+6.1D ${DIR_REGRESSOR}/
 mv ${DIR_SCRATCH}/${PREFIX}_moco+12.1D ${DIR_REGRESSOR}/
@@ -373,13 +366,8 @@ mv ${DIR_SCRATCH}/${PREFIX}_moco+warp.nii.gz \
   ${DIR_PROJECT}/derivatives/func/moco_${TEMPLATE}+${SPACE}/${PREFIX}_reg-${TEMPLATE}+${SPACE}_bold.nii.gz
 
 if [[ "${KEEP}" == "true" ]]; then
-  if [ "${IS_SES}" = true ]; then
-    mkdir -p ${DIR_PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}
-    mv ${DIR_SCRATCH}/* ${DIR_PROJECT}/derivatives/func/prep/sub-${SUBJECT}/ses-${SESSION}/
-  else
-    mkdir -p ${DIR_PROJECT}/derivatives/func/prep/sub-${SUBJECT}
-    mv ${DIR_SCRATCH}/* ${DIR_PROJECT}/derivatives/func/prep/sub-${SUBJECT}/
-  fi
+  mkdir -p ${DIR_PROJECT}/derivatives/func/prep/${DIR_SUBSES}
+  mv ${DIR_SCRATCH}/* ${DIR_PROJECT}/derivatives/func/prep/${DIR_SUBSES}/
 fi
 
 #===============================================================================
