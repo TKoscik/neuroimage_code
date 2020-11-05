e6d939feb63ea49042245cfb183bed32c094fc4b
koscikt <timothy-koscik@uiowa.edu>
2020-11-05 08:31:39 -0600

    Update bids/get_space.sh

--- bids/get_space.sh
+++ bids/get_space.sh
@@ -2,19 +2,16 @@
 
 #===============================================================================
 # Get the spacing label from BIDS-IA format
-#  -this function assumes images are in one of these directories"
-#    -*/rawdata/  [*/nifti/ for local, backward compatibility]
-#    -*/native/
-#    -*/*_to-${SPACE_LABEL}/
-#  - if none of these directories are true the function will return everything
-#    trailing the last "-" or "_" (whichever comes last) in the the name of the
-#    lowest level directory
+# 1. use reg field if exists in file name, or parse directory
+# 2. if reg_* exists in directory structure, pull space from reg_* folder name
+# 3. if in derivatives folder assume "native" space
+# 4. if in rawdata or nifti(deprecated) folder, assume "raw" space
 # Authors: Timothy R. Koscik, PhD
 # Date: 2020-03-12
 #===============================================================================
 
 # Parse inputs -----------------------------------------------------------------
-OPTS=`getopt -o hi: --long input:,help -n 'parse-options' -- "$@"`
+OPTS=$(getopt -o hi: --long input:,help -n 'parse-options' -- "$@")
 if [ $? != 0 ]; then
   echo "Failed parsing options" >&2
   exit 1
@@ -23,6 +20,7 @@ eval set -- "$OPTS"
 
 # Set default values for function ---------------------------------------------
 INPUT=
+DIR_INC=/Shared/inc_scratch/code
 HELP=false
 
 while true; do
@@ -36,12 +34,12 @@ done
 
 # Usage Help -------------------------------------------------------------------
 if [[ "${HELP}" == "true" ]]; then
-  FUNC_NAME=(`basename "$0"`)
+  FCN_NAME=(`basename "$0"`)
   echo ''
   echo '------------------------------------------------------------------------'
-  echo "Iowa Neuroimage Processing Core: ${FUNC_NAME}"
+  echo "Iowa Neuroimage Processing Core: ${FCN_NAME}"
   echo '------------------------------------------------------------------------'
-  echo "Usage: ${FUNC_NAME}"
+  echo "Usage: ${FCN_NAME}"
   echo '  -h | --help              display command help'
   echo '  -i | --input             file path to find BIDs Project directory'
   echo ''
@@ -51,10 +49,6 @@ fi
 #==============================================================================
 # Start of function
 #==============================================================================
-# 1. use reg field if exists in file name, or parse directory
-# 2. if reg_* exists in directory structure, pull space from reg_* folder name
-# 3. if in derivatives folder assume "native" space
-# 4. if in rawdata or nifti(deprecated) folder, assume "raw" space
 SPACE=$(${DIR_INC}/bids/get_field.sh -i ${INPUT} -f reg)
 if [[ -z ${SPACE} ]]; then
   DIR_INPUT=($(dirname ${INPUT}))
