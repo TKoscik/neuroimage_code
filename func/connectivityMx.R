c0281b3247f1b49264742f31aca00d948de216b3
koscikt <timothy-koscik@uiowa.edu>
2021-02-09 13:15:19 -0600

    Update func/connectivityMx.R

--- func/connectivityMx.R
+++ func/connectivityMx.R
@@ -1,1 +1,9 @@
+args <- commandArgs(trailingOnly = TRUE)
 
+for (i in 1:length(args)) {
+  if (args[i] %in% c("ts")) {
+    ts <- args[i+1]
+  } else if (args[i] %in% c("show", "show.plot", "plot")) {
+    show.plot <- TRUE
+  }
+}
