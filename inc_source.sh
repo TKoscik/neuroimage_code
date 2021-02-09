0e8fada7db73f7ab14e696783b363e686038d095
koscikt <timothy-koscik@uiowa.edu>
2021-02-09 10:11:15 -0600

    Update inc_source.sh, init.json, generic/modField.sh, generic/getSpace.sh, generic/getScanner.sh, generic/getProject.sh, generic/getField.sh, generic/getDir.sh, generic/getColumn.sh, generic/getBidsBase.sh, generic/convSpacing.sh files Deleted dicom/deprecated/dicom_conversion.sh, dicom/deprecated/dicom_sort.R, dicom/deprecated/qc_dicom_conversion.R, dicom/deprecated/scan_description.R files

--- inc_source.sh
+++ inc_source.sh
@@ -10,22 +10,26 @@ HARDWARE="$(uname -m)"
 # set version number for INC code ----------------------------------------------
 VERSION="$1"
 if [[ -z ${VERSION} ]]; then
-  VERSION="0.0.0.0"
+  VERSION="dev"
 fi
-echo "Setting up Iowa Neuroimage Processing Core Software version ${VERSION}"
 
 # locate init.json -------------------------------------------------------------
-#DIR_INIT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#INIT=/Shared/pinc/sharedopt/apps/inc/${KERNEL}/${HARDWARE}/${VERSION}/init.json
-INIT=/Shared/inc_scratch/dev_code/init.json
+if [[ "${VERSION}" == "dev" ]]; then
+  INIT=/Shared/inc_scratch/dev_code/init.json
+else
+  INIT=/Shared/pinc/sharedopt/apps/inc/${KERNEL}/${HARDWARE}/${VERSION}/init.json
+fi
 if [[ ! -f ${INIT} ]]; then
   echo "file not found: ${INIT}"
   exit 1
 fi
+echo "Setting up Iowa Neuroimage Processing Core Software - Version ${VERSION^^}"
+export DIR_INC=$(dirname ${INIT})
 
 # load JQ to read init info from JSON ------------------------------------------
 if [[ ! -x "$(command -v jq)" ]]; then
   if [[ "${HOSTNAME}" == "argon" ]]; then
+    echo -e "\tloading JSON modules"
     module load stack/2020.2 jq/1.6_gcc-8.4.0
   fi
   if [[ ! -x "$(command -v jq)" ]]; then
@@ -37,7 +41,7 @@ fi
 # export directories -----------------------------------------------------------
 echo "EXPORTING DIRECTORIES:"
 PATH_VARS=($(jq -r '.export_paths | keys_unsorted' < ${INIT} | tr -d ' [],"'))
-for (( i=0; i<${#PATHS[@]}; i++ )); do
+for (( i=0; i<${#PATH_VARS[@]}; i++ )); do
   PATH_STR=($(jq -r ".export_paths.${PATH_VARS[${i}]}" < ${INIT} | tr -d ' [],"'))
   export ${PATH_VARS[${i}]}=${PATH_STR}
   echo -e "\t${PATH_VARS[${i}]}=${PATH_STR}"
@@ -47,10 +51,10 @@ done
 echo "LOADING SOFTWARE DEPENDENCIES:"
 SW_LS=($(jq -r '.software | keys_unsorted' < ${INIT} | tr -d ' [],"'))
 for (( i=0; i<${#SW_LS[@]}; i++ )); do
-  unset SW_NAME VERSION CMDS
+  unset SW_NAME SW_VERSION CMDS
   SW_NAME=${SW_LS[${i}]}
   WHICH_HOST=($(jq -r ".software.${SW_NAME}.hostname" < ${INIT} | tr -d ' [],"'))
-  VERSION=($(jq -r ".software.${SW_NAME}.version" < ${INIT} | tr -d ' [],"'))
+  SW_VERSION=($(jq -r ".software.${SW_NAME}.version" < ${INIT} | tr -d ' [],"'))
   CMDS=($(jq -r ".software.${SW_NAME}.command" < ${INIT} | tr -d ' [],"'))
   if [[ "${WHICH_HOST}" == "${HOSTNAME}" ]] |
      [[ "${WHICH_HOST}" == "all" ]]; then
@@ -60,13 +64,13 @@ for (( i=0; i<${#SW_LS[@]}; i++ )); do
       fi
     done
   fi
-  echo -e "\t${SW_NAME^^}/${VERSION}"
+  echo -e "\t${SW_NAME^^}/${SW_VERSION}"
 done
 
 # setup R packages -------------------------------------------------------------
 # run manually for now
-#Rscript /Shared/pinc/sharedopt/apps/inc/${KERNEL}/${HARDWARE}/${INC_VERSION}/r_setup.R
-echo "INC CODE version ${INC_VERSION} has been setup."
+#Rscript ${DIR_INC}/r_setup.R
+echo "INC CODE version ${VERSION^^} has been setup."
 echo "If you haven't setup your R environment, please run:"
-echo "Rscript /Shared/pinc/sharedopt/apps/inc/${KERNEL}/${HARDWARE}/${INC_VERSION}/r_setup.R"
+echo "Rscript ${DIR_INC}/r_setup.R"
 
