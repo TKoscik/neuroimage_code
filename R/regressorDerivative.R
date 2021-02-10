17044602f5299a3cda3bbd5d53c7f5eeed639efe
koscikt <timothy-koscik@uiowa.edu>
2021-02-10 15:42:11 -0600

    Update R/regressorConvert.R, R/regressorDerivative.R, R/regressorDisplacement.R, R/regressorQuadratic.R files

--- R/regressorDerivative.R
+++ R/regressorDerivative.R
@@ -4,4 +4,4 @@ library(tools)
 
 df <- read.csv(args[1], header=F)
 for (i in 1:ncol(df)) { df[ ,i] <- c(0, diff(df[ ,i])) }
-write.table(df, filename=paste0(args[2], basename(file_path_sans_ext(args[1])), "+deriv.1D"), quotes=F, row.names=F, col.names=F, sep=" ")
+write.table(df, file=paste0(args[2], basename(file_path_sans_ext(args[1])), "+deriv.1D"), quotes=F, row.names=F, col.names=F, sep=" ")
